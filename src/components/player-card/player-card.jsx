3efc1ef78e36306b586cf383c14d72df63b03eaa
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-06-03 11:40:06 +0300

    WAI-11 Input a character for another player * add coment to the Player-card Component

--- src/components/player-card/player-card.jsx
+++ src/components/player-card/player-card.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './player-card.scss';
 
+// <-- Use this names for Avavr icons -->
+  // 'avatar01'
+  // 'avatar02'
+  // 'avatar03'
+  // 'avatar04'
+
+  // <-- Use this names for Player Satus -->
+  // 'wait'
+  // 'yes'
+  // 'no'
+
+  // if you don't asined description and player status it will be hidden 
+
 function PlayerCard({
   avatar,
   name,
