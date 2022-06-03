66a04e9911462ca826c61be0346a4cf3f15101ae
Maksym Rudenko <maksym.rudenko@eleks.com>
2022-06-03 15:24:21 +0300

    fixed yarn.lock errors

--- src/components/btn/btn.jsx
+++ src/components/btn/btn.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import './btn.scss';
 
 // <-- Use this names for Button Styles -->
-  // 'btn-green-solid'
-  // 'btn-pink-solid'
-  // 'btn-yellow-solid'
-  // 'btn-disabled'
-  // 'btn-lightgray-outline'
+// 'btn-green-solid'
+// 'btn-pink-solid'
+// 'btn-yellow-solid'
+// 'btn-disabled'
+// 'btn-lightgray-outline'
 
-
-function Btn({ children, type, onClick, btnStyle='btn-green-solid' }) {
-
+function Btn({ children, type, onClick, btnStyle = 'btn-green-solid' }) {
   return (
     <button className={`btn ${btnStyle}`} onClick={onClick} type={type}>
       {children}
