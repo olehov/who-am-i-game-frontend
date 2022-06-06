884c05b92552e9ec23f79e7d2257084de58e9ecd
Olha Safroniuk <olha.safroniuk@eleks.com>
2022-06-06 18:04:04 +0300

    change Layout display

--- src/components/main-layout/main-layout.jsx
+++ src/components/main-layout/main-layout.jsx
@@ -6,10 +6,11 @@ function MainLayout(props) {
   return (
     <div className="layout">
       <Header>
-        {props.button && (
-          <Btn btnStyle="btn-pink-solid button_leave_game">Leave game</Btn>
+        {props.timer ? (
+          <div>timer</div>
+        ) : (
+          <Btn btnStyle="btn-leave-game">Leave game</Btn>
         )}
-        {props.timer && <div>timer</div>}
       </Header>
       <div className="content_wrapper">{props.children}</div>
     </div>
