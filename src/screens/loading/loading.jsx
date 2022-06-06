d26d2b196d6d0557c3d3719345073701c001ba74
Maksym Rudenko <maksym.rudenko@eleks.com>
2022-06-06 18:50:00 +0300

    JCWAIG-12 fixed loading page modal

--- src/screens/loading/loading.jsx
+++ src/screens/loading/loading.jsx
@@ -1,9 +1,13 @@
 import GameTitle from '../../components/game-title/game-title';
 import LeaveGameBtn from '../../components/leave-game-btn/leave-game-btn';
 import Timer from '../../components/timer/timer';
+import LeaveGameModal from '../../components/modals/leave-game/leave-game';
+import { useState } from 'react';
 import './loading.scss';
 
 function Loading() {
+  const [modalActive, setModalActive] = useState(false);
+
   return (
     <div className="loading">
       <GameTitle />
@@ -12,7 +16,8 @@ function Loading() {
         PLEASE WAIT UNTIL WE FIND YOUR OPPONENTS
       </h3>
       <Timer />
-      <LeaveGameBtn page={'loading-page'} />
+      <LeaveGameBtn page={'loading-page'} setModalActive={setModalActive} />
+      <LeaveGameModal showModal={modalActive} setModalActive={setModalActive} />
     </div>
   );
 }
