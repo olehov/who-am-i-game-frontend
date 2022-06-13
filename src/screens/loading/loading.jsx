dcbd0eab82450338827b7ff7a5916bd5b86726a9
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-06-13 20:02:54 +0300

    feature/replace-buttons * replace buttons

--- src/screens/loading/loading.jsx
+++ src/screens/loading/loading.jsx
@@ -1,26 +1,38 @@
 import GameTitle from '../../components/game-title/game-title';
-import LeaveGameBtn from '../../components/leave-game-btn/leave-game-btn';
 import Timer from '../../components/timer/timer';
-import LeaveGameModal from '../../components/modals/leave-game/leave-game';
-import { useState } from 'react';
+import GuessCharacterModal from '../../components/modals/guess-a-character/guess-a-character';
 import './loading.scss';
+import { useState } from 'react';
+import Btn from '../../components/btn/btn';
+import LeaveGameModal from '../../components/modals/leave-game/leave-game';
 
 function Loading() {
+  const [displayModal, setDisplayModal] = useState(true);
   const [modalActive, setModalActive] = useState(false);
 
   return (
     <div className="loading">
-      <GameTitle />
-      <h3 className="loading__queue-number">23 PLAYERS ARE IN QUEUE</h3>
-      <h3 className="loading__waiting-message">
-        PLEASE WAIT UNTIL WE FIND YOUR OPPONENTS
-      </h3>
-      <Timer />
-      <LeaveGameBtn
-        className={'loading-page'}
-        setModalActive={setModalActive}
-      />
-      <LeaveGameModal showModal={modalActive} setModalActive={setModalActive} />
+      <div className="loading__wrapper">
+        <GameTitle />
+        <h3 className="loading__queue-number">23 PLAYERS ARE IN QUEUE</h3>
+        <h3 className="loading__waiting-message">
+          PLEASE WAIT UNTIL WE FIND YOUR OPPONENTS
+        </h3>
+        <Timer />
+        <div className="loading__btn-wrap">
+          <Btn onClick={() => setModalActive(true)} className="btn-pink-solid">
+            Leave Game
+          </Btn>
+        </div>
+        <LeaveGameModal
+          showModal={modalActive}
+          setModalActive={setModalActive}
+        />
+        <GuessCharacterModal
+          displayModal={displayModal}
+          setDisplayModal={setDisplayModal}
+        />
+      </div>
     </div>
   );
 }
