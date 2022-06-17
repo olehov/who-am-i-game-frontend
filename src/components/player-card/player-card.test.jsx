1bfd2581c4ba65f698eca92a1b15b1ccfc36c4fd
Olha Safroniuk <olha.safroniuk@eleks.com>
2022-06-17 17:29:55 +0300

    add player-card component tests

--- src/components/player-card/player-card.test.jsx
+++ src/components/player-card/player-card.test.jsx
@@ -1,20 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import UserItem from './user-item';
+import PlayerCard from './player-card';
 
-describe('UserItem Component', () => {
+describe('PlayerCard Component', () => {
   const user = { name: 'GreenDean', character: 'Tom Ford' };
 
   it('displays a user name', () => {
-    render(<UserItem user={user} />);
+    render(<PlayerCard name={user.name} assignedCharacter={user.character} />);
     const userName = screen.getByText(/GreenDean/);
     expect(userName).toBeInTheDocument();
-    expect(userName).toHaveAttribute('class', 'user_item_name');
   });
 
   it('displays a user character', () => {
-    render(<UserItem user={user} />);
+    render(<PlayerCard name={user.name} assignedCharacter={user.character} />);
     const userCharacter = screen.getByText(/Tom Ford/);
     expect(userCharacter).toBeInTheDocument();
-    expect(userCharacter).toHaveAttribute('class', 'user_item_character');
   });
 });
