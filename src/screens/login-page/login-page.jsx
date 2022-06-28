2c620d1097ac8eb4e096af61d7422603929bd912
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-06-28 16:41:52 +0300

    feature/login-screen * fix imports

--- src/screens/login-page/login-page.jsx
+++ src/screens/login-page/login-page.jsx
@@ -2,14 +2,14 @@ import GameTitle from '../../components/game-title/game-title';
 import Btn from '../../components/btn/btn';
 import ScreenWrapper from '../../components/wrappers/screen-wrapper/screen-wrapper';
 import PlayersOnlineTitle from '../../components/players-online-title/players-online-title';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import GameDataContext from '../../contexts/game-data-context';
+import { createGame } from '../../services/games-service';
+import { NUMBER_OF_PLAYERS } from '../../constants/constants';
 import './login-page.scss';
-// import { useContext } from 'react';
-// import GameDataContext from '../../contexts/game-data-context';
 
 function LoginPage() {
-  // const { resetData } = useContext(GameDataContext);
-  const navigate = useNavigate();
+  const { setGameData, playerId } = useContext(GameDataContext);
 
   return (
     <ScreenWrapper>
@@ -18,17 +18,19 @@ function LoginPage() {
       <Btn
         className={'btn-blue-outline'}
         onClick={async () => {
-          // setGameData(await createGame(playerId, NUMBER_OF_PLAYERS));
+          setGameData(await createGame(playerId, NUMBER_OF_PLAYERS));
         }}
       >
         PLAY QUICK GAME
       </Btn>
       <div className="dividing-line"></div>
       <Btn className={'btn-blue-outline'}>CREATE ACCOUNT</Btn>
-      <div className={"text-login or"}>or</div>
-      <Btn className={'btn-fb-blue'} iconClassName={'fb'}>Continue with Facebook</Btn>
-      <div className={"dividing-line"}></div>
-      <div className={"text-login already"}>Already have a account?</div>
+      <div className={'text-login or'}>or</div>
+      <Btn className={'btn-fb-blue'} iconClassName={'fb'}>
+        Continue with Facebook
+      </Btn>
+      <div className={'dividing-line'}></div>
+      <div className={'text-login already'}>Already have a account?</div>
       <Btn className={'btn-blue-outline'}>SIGN IN</Btn>
     </ScreenWrapper>
   );
