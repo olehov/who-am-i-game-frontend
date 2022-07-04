f487c2abb49d5c500b1837c17defa6d5cf4de30d
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-07-04 11:50:46 +0300

    feature/login-screen * add className to Input , delete ID

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,13 @@
 import clsx from 'clsx';
 import './input.scss';
 
-const Input = ({ id, type, error, ...attrs }) => {
-  const classes = clsx('input', { error });
+const Input = ({ className, type, error, ...attrs }) => {
+  const classes = clsx('input', className, { error });
 
   return (
     <div className="input-container">
       <div className="input-wrapper">
-        <input className={classes} name={id} id={id} {...attrs} />
+        <input className={classes} {...attrs} />
         {type === 'password' && <div className="eye-icon"></div>}
       </div>
       {error && <div className="inputError">{error}</div>}
